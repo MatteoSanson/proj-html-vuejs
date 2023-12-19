ebec3b7cc1fd18493615cc1edf09633e909b73bb
MatteoSanson <matteo.sanson@gmail.com>
2023-12-19 19:15:54 +0100

    feat: add buttonheader

---
+++ src/components/header/buttonHeader.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+    name: 'buttonHeader',
+};
+</script>
+
+<template>
+    <button>REGISTER NOW</button>
+</template>
+
+<style scoped lang="scss">
+@use '../../assets/scss/partials/variables.scss' as *;
+
+button {
+    width: 195px;
+    height: 60px;
+    background-color: $bg-button-primary;
+    border: none;
+    color: $secondary-color;
+    cursor: pointer;
+}
+</style>
