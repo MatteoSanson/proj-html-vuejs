dcbe3a7cdc1beddfa9b2c17ef3d99d9a658fc56d
MatteoSanson <matteo.sanson@gmail.com>
2023-12-20 14:12:08 +0100

    feat: component learncontent

--- src/components/main/learnContent.vue
+++ src/components/main/learnContent.vue
@@ -1,12 +1,40 @@
 <script>
 export default {
     name: 'learnContent',
+    data() {
+        return {
+            info: {
+                title: 'Learning Possibilities',
+                text: 'Lorem ipsum gravida nibh vel velit auctor aliquetn sollicitudirem quibibendum auci elit cons equat ipsutis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt.',
+            },
+            options: [
+                'We enrich lives through learning.',
+                'Maximizing potential through individual attention.',
+                'The trusted name for specialized training.',
+                'People teach. People learn. This is where they connect.',
+            ]
+        }
+    }
 };
 </script>
 
 <template>
     <div class="containerCo">
-        ciao
+        <h2>{{ info.title }}</h2>
+        <p>{{ info.text }}</p>
+        <ul>
+            <li v-for="option in options">
+                <div class="left">
+                    <font-awesome-icon class="check-icon" icon="fa-solid fa-check" />
+                </div>
+                <div class="right">
+                    <span>{{ option }}</span>
+                </div>
+            </li>
+        </ul>
+        <div class="bottom-right-image">
+            <img src="../../assets/icademy/pics/h12-tabs-icon-1.png" alt="tabs icon">
+        </div>
     </div>
 </template>
 
@@ -17,5 +45,50 @@ export default {
 
 .containerCo {
     flex-grow: 1;
+    padding: 0 25px 0 15px;
+    position: relative;
+
+    h2 {
+        font-size: 35px;
+        padding-top: 10px;
+    }
+
+    p {
+        color: $sixth-color;
+        font-size: 18px;
+        line-height: 30px;
+        padding: 20px 0 35px;
+    }
+
+    ul {
+        color: $sixth-color;
+        width: 50%;
+
+        li {
+            padding-bottom: 25px;
+            display: flex;
+
+            .left {
+                padding-top: 5px;
+            }
+
+            .check-icon {
+                color: $fourth-color;
+                margin-right: 10px;
+            }
+
+            span {
+                line-height: 30px;
+            }
+        }
+    }
+
+    .bottom-right-image {
+        position: absolute;
+        bottom: 0;
+        right: 0;
+        // margin-bottom: 20px;
+        // margin-right: 20px; 
+    }
 }
 </style>
