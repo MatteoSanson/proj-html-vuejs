12e6b5b7ed0cf55c3086d87b95d0182b49d2fca6
MatteoSanson <matteo.sanson@gmail.com>
2023-12-20 12:22:06 +0100

    feat: add components

---
+++ src/components/main/learning.vue
@@ -0,0 +1,30 @@
+<script>
+import learnNav from './learnNav.vue';
+import learnContent from './learnContent.vue';
+export default {
+    name: 'learning',
+    components: {
+        learnNav,
+        learnContent,
+    }
+};
+</script>
+
+<template>
+    <div class="container">
+        <learnNav />
+        <learnContent />
+    </div>
+</template>
+
+<style scoped lang="scss">
+@use '../../assets/scss/partials/variables.scss' as *;
+@use '../../assets/scss/partials/mixins.scss' as *;
+
+.container {
+    @include container;
+    display: flex;
+    gap: 40px;
+    padding: 130px 0;
+}
+</style>
