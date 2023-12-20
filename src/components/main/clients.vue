03e5ba2736057f0a8905efcacd3bc7eb5ceb1e10
MatteoSanson <matteo.sanson@gmail.com>
2023-12-20 22:52:32 +0100

    feat: component clients

---
+++ src/components/main/clients.vue
@@ -0,0 +1,77 @@
+<script>
+export default {
+    name: 'clients',
+    data() {
+        return {
+            list: [
+                {
+                    name: 'iAcademy logo',
+                    img: '../src/assets/icademy/pics/h5-client-5.png',
+                    link: '#',
+                    target: '_self',
+                },
+                {
+                    name: 'Study Central logo',
+                    img: '../src/assets/icademy/pics/h5-client-1.png',
+                    link: '#',
+                    target: '_self',
+                },
+                {
+                    name: 'Educator Logo',
+                    img: '../src/assets/icademy/pics/h5-client-2.png',
+                    link: '#',
+                    target: '_self',
+                },
+                {
+                    name: 'Study Central logo',
+                    img: '../src/assets/icademy/pics/h5-client-1.png',
+                    link: '#',
+                    target: '_self',
+                },
+            ]
+        }
+    }
+};
+</script>
+
+<template>
+    <div class="container">
+        <ul>
+            <li v-for="li in list">
+                <div class="img-cnt">
+                    <a :href="li.link" :target="li.target">
+                        <img :src="li.img" :alt="li.name">
+                    </a>
+                </div>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<style scoped lang="scss">
+@use '../../assets/scss/partials/variables.scss' as *;
+@use '../../assets/scss/partials/mixins.scss' as *;
+
+.container {
+    @include container;
+    padding: 80px 0;
+
+    ul {
+        display: flex;
+
+        li {
+            width: calc(100% / 4);
+
+            .img-cnt {
+                width: 100%;
+
+                img {
+                    width: 100%;
+                }
+            }
+        }
+    }
+
+
+}
+</style>
