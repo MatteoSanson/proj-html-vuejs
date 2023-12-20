9ab8bd310f7331e1b7be14a9b054cd374dc5c891
MatteoSanson <matteo.sanson@gmail.com>
2023-12-20 12:22:22 +0100

    feat: prova comp. learncontent

---
+++ src/components/main/learnContent.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+    name: 'learnContent',
+};
+</script>
+
+<template>
+    <div class="containerCo">
+        ciao
+    </div>
+</template>
+
+<style scoped lang="scss">
+@use '../../assets/scss/partials/variables.scss' as *;
+@use '../../assets/scss/partials/mixins.scss' as *;
+
+
+.containerCo {
+    flex-grow: 1;
+}
+</style>
