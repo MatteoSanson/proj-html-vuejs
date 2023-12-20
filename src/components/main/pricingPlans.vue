f1b9296a3fa0d5267f3adf40ca51640f4284c92a
MatteoSanson <matteo.sanson@gmail.com>
2023-12-20 16:15:18 +0100

    feat: components pricingPlans

---
+++ src/components/main/pricingPlans.vue
@@ -0,0 +1,38 @@
+<script>
+import intestazione from './intestazione.vue';
+export default {
+    name: 'pricingPlans',
+    components: {
+        intestazione,
+    },
+    data() {
+        return {
+            title: 'Pricing Plans',
+            description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea magni error totam, suscipit labore mollitia!',
+        };
+    },
+};
+</script>
+
+<template>
+    <div class="mega-container">
+        <div class="container">
+            <intestazione :title="title" :description="description" />
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+@use '../../assets/scss/partials/variables.scss' as *;
+@use '../../assets/scss/partials/mixins.scss' as *;
+
+.mega-container {
+    background-image: url('../../assets/icademy/pics/page-background-img.png');
+    border-bottom: 1px solid $primary-bd-color;
+
+    .container {
+        @include container;
+        text-align: center;
+    }
+}
+</style>
