edd5c7ec7b7f8cb6a03a8066eec46cd9657b2cc2
MatteoSanson <matteo.sanson@gmail.com>
2023-12-21 11:55:11 +0100

    feat: component bottomFooter

---
+++ src/components/footer/bottomFooter.vue
@@ -0,0 +1,67 @@
+<script>
+export default {
+    name: 'bottomFooter',
+};
+</script>
+
+<template>
+    <div class="container">
+        <div class="left">
+            <p>Copyrights 2017 &copy; Mikado Themes</p>
+        </div>
+        <div class="right">
+            <ul>
+                <li>
+                    <a href="#" target="_self">
+                        <p>CALL +44 300 303 0266</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" target="_self">
+                        <p>FOLLOW US</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" target="_self">
+                        <p><font-awesome-icon icon="fa-brands fa-twitter" /></p>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" target="_self">
+                        <p><font-awesome-icon icon="fa-brands fa-instagram" /></p>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" target="_self">
+                        <p><font-awesome-icon icon="fa-brands fa-facebook-f" /></p>
+                    </a>
+                </li>
+
+            </ul>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+@use '../../assets/scss/partials/variables.scss' as *;
+@use '../../assets/scss/partials/mixins.scss' as *;
+
+.container {
+    @include container;
+    padding: 40px 0;
+    display: flex;
+    justify-content: space-between;
+    color: $fifth-color;
+
+    p {
+        font-size: 12px;
+    }
+
+    .right {
+        ul {
+            display: flex;
+            gap: 15px;
+        }
+    }
+}
+</style>
