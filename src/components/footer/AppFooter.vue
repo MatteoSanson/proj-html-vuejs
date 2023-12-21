a48c8657c69d20dec926a3731e2eeedec9db4610
MatteoSanson <matteo.sanson@gmail.com>
2023-12-21 11:28:36 +0100

    feat: add components

--- src/components/footer/AppFooter.vue
+++ src/components/footer/AppFooter.vue
@@ -1,15 +1,28 @@
 <script>
+import topFooter from './topFooter.vue';
+import bottomFooter from './bottomFooter.vue';
 export default {
     name: 'AppFooter',
+    components: {
+        topFooter,
+        bottomFooter,
+    }
 };
 </script>
 
 <template>
     <footer>
-        --- Footer ---
+        <topFooter />
+        <bottomFooter />
     </footer>
 </template>
 
 <style scoped lang="scss">
 @use '../../assets/scss/partials/variables.scss' as *;
+@use '../../assets/scss/partials/mixins.scss' as *;
+
+footer {
+    background-color: $bg-footer;
+    color: $tertiary-color;
+}
 </style>
